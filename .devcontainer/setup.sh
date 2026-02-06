73c7688e9b3939d78432205ed8cf359016c67d9e
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-05 18:22:18 -0800

    Testing without arch pt 2

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+/set -eux
 echo "---------- Running Setup"
+
+# Login to Azure
+az login
+
 curl -LJO https://raw.githubusercontent.com/luciendberkeley/arch-codespace-template/refs/heads/main/.config/.vimrc
 mv ./.vimrc ~/
 
+terraform plan
