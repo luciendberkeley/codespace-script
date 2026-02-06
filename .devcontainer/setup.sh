2731612991eeb5ff8acc2ae8682b5040851dc829
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-05 18:33:09 -0800

    aeisntoirent

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,10 +3,10 @@
 set -eux
 echo "---------- Running Setup"
 
-# Login to Azure
-az login
+sudo apt update
+sudo apt install -y vim nodejs npm
 
 curl -LJO https://raw.githubusercontent.com/luciendberkeley/arch-codespace-template/refs/heads/main/.config/.vimrc
 mv ./.vimrc ~/
 
-terraform plan
+echo "Setup done"
