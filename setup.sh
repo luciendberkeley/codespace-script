2e46311288c0806122ba6764d52cd8a1014ce808
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-05 17:39:36 -0800

    continuing

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "Running Setup"
+echo "---------- Running Setup"
 curl -LJO https://raw.githubusercontent.com/luciendberkeley/arch-codespace-template/refs/heads/main/.config/.vimrc
+mv ./.vimrc ~/
+
