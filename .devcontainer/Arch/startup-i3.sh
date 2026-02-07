5e423cca79f95df85ecc4371cf16d9c27493bcb8
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-07 06:35:25 +0000

    Copying the startup script

--- .devcontainer/Arch/startup-i3.sh
+++ .devcontainer/Arch/startup-i3.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bash
-set -e
 
 echo "Starting virtual display..."
-Xvfb :0 -screen 0 1024x768x24 &
+Xvfb :1 -screen 0 1280x800x24 &
 
-export DISPLAY=:0
+export DISPLAY=:1
 
 echo "Starting i3 window manager..."
-su - arch -c "i3 &"
+i3 &
 
 echo "Starting VNC server..."
-x11vnc -display :0 -nopw -forever -shared -rfbport 5900 &
+x11vnc -display :1 -forever -nopw -listen localhost -rfbport 5900 &
+
 
 echo "Starting noVNC on port 6080..."
-/usr/share/noVNC/utils/novnc_proxy.sh --vnc localhost:5900 --listen 0.0.0.0:6800 --web /usr/share/noVNC &
+/usr/share/noVNC/utils/novnc_proxy --vnc localhost:5900 --listen 0.0.0.0:6080 &
 
 echo ""
 echo "GUI environment is ready!"
