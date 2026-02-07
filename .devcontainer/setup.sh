cdc2baf58a6706c66b2e46c5f07b6efcd42a774b
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-06 16:57:47 -0800

    Testing arch alone

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,10 +3,10 @@
 set -eux
 echo "---------- Running Setup"
 
-sudo pacman -Syu
-sudo pacman --noconfirm vim tmux kitty
+# sudo pacman -Syu
+# sudo pacman --noconfirm vim tmux kitty
 
-curl -LJO https://raw.githubusercontent.com/luciendberkeley/codespace-script/refs/heads/main/.config/.vimrc
-mv ./.vimrc ~/
+# curl -LJO https://raw.githubusercontent.com/luciendberkeley/codespace-script/refs/heads/main/.config/.vimrc
+# mv ./.vimrc ~/
 
 echo "Setup done"
