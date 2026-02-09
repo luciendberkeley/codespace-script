755e63c33b0234caaf2205508a97eef64e83d590
luciendberkeley <lucienduchateau@students.berkeley.net>
2026-02-09 00:01:16 +0000

    Added vimium config

--- .devcontainer/Arch/setup.sh
+++ .devcontainer/Arch/setup.sh
@@ -9,6 +9,8 @@ sudo pacman -S --noconfirm --needed vim tmux kitty firefox git jdk-openjdk base-
 curl -LJO https://raw.githubusercontent.com/luciendberkeley/codespace-script/refs/heads/main/.config/.vimrc
 mv ./.vimrc ~/
 
+curl -LJO https://raw.githubusercontent.com/luciendberkeley/codespace-script/refs/heads/main/.config/vimium-config.json
+
 export DISPLAY=:1
 
 echo "Setup done"
